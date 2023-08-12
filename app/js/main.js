26bd67cf37878163fddc1e7fdef44758042defd2
molexandev01~ <molexandev@gmail.com>
2023-08-12 18:41:58 +0300

    finished the desktop version

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
 $(function(){
    
+   // SLIDER
+
+   $(".happy-clients__slider-wrapper").slick({
+      dots: false,
+      arrows: true,
+      speed: 1000,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      slidesToScroll: 1,
+      slidesToShow: 2,
+      draggable: true,
+  });
+
 })
