93bdf34248d0a5fde129ea6be706926617b679d9
molexandev01~ <molexandev@gmail.com>
2023-08-13 17:49:00 +0300

    added several sections adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,29 @@
-$(function(){
-   
-   // SLIDER
+$(function () {
+    // SLIDER
 
-   $(".happy-clients__slider-wrapper").slick({
-      dots: false,
-      arrows: true,
-      speed: 1000,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      slidesToScroll: 1,
-      slidesToShow: 2,
-      draggable: true,
-  });
+    $(".happy-clients__slider-wrapper").slick({
+        dots: false,
+        arrows: true,
+        speed: 1000,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToScroll: 1,
+        slidesToShow: 2,
+        draggable: true,
+    });
 
-})
+    // MOBILE MENU
+
+    $(".menu__btn, .menu a").on("click", function () {
+        $(".menu__btn").toggleClass("menu__btn--active");
+        $(".menu__list").toggleClass("menu__list--active");
+        $("body").toggleClass("lock");
+    });
+
+    // FooterSlideToggle
+
+    $(".footer-top__title").on("click", function () {
+        $(this).toggleClass("footer-top__title--active");
+        $(this).next().slideToggle();
+    });
+});
