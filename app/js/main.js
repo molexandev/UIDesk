e3f0f3e867b050439fa18d9b5787cedcac72bc62
molexandev01~ <molexandev@gmail.com>
2023-08-15 18:25:46 +0300

    the project is finally completed

--- app/js/main.js
+++ app/js/main.js
@@ -48,4 +48,13 @@ $(function () {
       )
       wow.init();
 
+      // SLOW sCROLL
+
+      $("#menu").on("click", "a", function (e) {
+        e.preventDefault();
+        var id = $(this).attr("href"),
+            top = $(id).offset().top;
+        $("body,html").animate({ scrollTop: top }, 1500);
+    });
+
 });
